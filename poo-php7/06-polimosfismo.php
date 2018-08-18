d808c147e38fe4174f4b6be7bb5bcb05bc8a64f2
julian <farfan730@hotmail.com>
2018-08-17 21:02:34 -0500

    figuras geometricas

--- poo-php7/06-polimosfismo.php
+++ poo-php7/06-polimosfismo.php
@@ -1,16 +1,25 @@
 <?php
 
+/////////////////////////////////////
 
 interface Forma{
 	public function getArea();
 }
+
+/////////////////////////////////////
 
 class Circulo implements Forma{
 	private $radio;
 	public function __construct($r){
 		$this->radio=$r;
 	}
+
+	 public function getArea(){
+ 	 return $this->radio*pi();
+ }
 }
+
+/////////////////////////////////////
 
 class Rectangulo implements Forma{
 	private $altura;
@@ -20,6 +29,21 @@ class Rectangulo implements Forma{
 		$this->altura=$a;
 		$this->base=$b;
 	}
+	public function getArea(){
+ 	 return $this->base * $this->altura;
+ }
+
 }
+
+/////////////////////////////////////
+// creamos los objetos
+
+   $rectan1=new Rectangulo(3,3);
+   echo "\n\nEl area del rectangulo es : ".$rectan1->getArea();
+
+   $circulo1=new Circulo(8);
+   echo "\n\nArea del circulo: ".$circulo1->getArea();
+
+
 
 ?>
