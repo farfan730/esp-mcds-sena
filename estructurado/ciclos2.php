a34d45a83a004070d4633a7c5c50e693d762712e
julian <farfan730@hotmail.com>
2018-08-28 21:05:33 -0500

    factorial

--- estructurado/ciclos2.php
+++ estructurado/ciclos2.php
@@ -1,7 +1,28 @@
 <?php
 
+
+///////imprime multiplos del 5 hasta el 50////////
+/*
 for ($n=0; $n <=50 ; $n++) { 
 	  echo "".($n % 5==0) ? "-".$n: "";  
 }
+
+*/
+
+////////imprimir factorial de 1 a 5 ///////////////
+
+$factorial=5;
+$n=1;
+
+
+for ($i=0; $i <=5 ; $i++)
+{ 
+    if($n<=$factorial){
+    	 echo "".$i;
+    }
+}
+
+
+
 
 ?>
